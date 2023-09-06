ace68fd9169a21fb92512e1239ff3ff119df4f3b
Weylin76 <Weylin76@msn.com>
2023-09-05 21:25:57 -0400

    import data weeks 11/12

--- Assignments/Week12/chatgpt.py
+++ Assignments/Week12/chatgpt.py
@@ -1,18 +1,5 @@
+# Load the dataset in your Python program
 master_list = []
-min_age = 999
-min_age_year = 0
-min_age_country = ""  # Variable to store the country with min age
-max_age = 0
-max_age_year = 0
-max_age_country = ""  # Variable to store the country with max age
-
-# varialbes for user search
-user_min_age = 999
-user_min_county = ''
-user_min_age_year = 9999
-
-user_year = int(input("Enter the year of interest: "))
-
 
 with open('life-expectancy.csv') as life:
     header = next(life)  # Read and skip the header line
@@ -20,21 +7,38 @@ with open('life-expectancy.csv') as life:
     for line in life:
         line = line.strip()
         parts = line.split(",")
-        master_list.append(line)
-        country = parts[0]
-        acronym = parts[1]
-        year = int(parts[2])  # year as a int
-        age = float(parts[3])  # Age as a float
+        master_list.append(parts)
 
-        if age < min_age:
-            min_age = age
-            min_age_country = country
-            min_age_year = year
+# Get user input for the year of interest
+user_year = int(input("Enter the year of interest: "))
 
-        if age > max_age:
-            max_age = age
-            max_age_country = country
-            max_age_year = year
+# Find the overall max and min life expectancy and corresponding years
+overall_max_life = float('-inf')
+overall_max_country = ""
+overall_max_year = 0
+overall_min_life = float('inf')
+overall_min_country = ""
+overall_min_year = 0
+
+for row in master_list:
+    country = row[0]
+    year = int(row[2])
+    life_expectancy = float(row[3])
+
+    if life_expectancy > overall_max_life:
+        overall_max_life = life_expectancy
+        overall_max_country = country
+        overall_max_year = year
+
+    if life_expectancy < overall_min_life:
+        overall_min_life = life_expectancy
+        overall_min_country = country
+        overall_min_year = year
+
+print(
+    f"The overall max life expectancy is: {overall_max_life:.3f} from {overall_max_country} in {overall_max_year}")
+print(
+    f"The overall min life expectancy is: {overall_min_life:.3f} from {overall_min_country} in {overall_min_year}")
 
 # Find and display information for the user's specified year
 total_life_expectancy = 0
@@ -69,6 +73,3 @@ print(
     f"The max life expectancy was in {max_country} with {max_life_expectancy:.2f}")
 print(
     f"The min life expectancy was in {min_country} with {min_life_expectancy:.3f}")
-
-print(f'Max Age: {min_age} Country: {min_age_country} year: {min_age_year}')
-print(f'Max Age: {max_age} Country: {max_age_country} year: {max_age_year}')
