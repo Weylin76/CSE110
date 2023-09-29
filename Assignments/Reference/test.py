6274f38b331be4485323b09258e9d7bbe75ed02f
Weylin76 <Weylin76@msn.com>
2023-09-28 22:50:56 -0400

    playing with code

--- Assignments/Reference/test.py
+++ Assignments/Reference/test.py
@@ -1,8 +1,2 @@
-# # List of words
-# words = 'bird'
 
-# # Join the words with a space separator
-# sentence = ''.join(words)
-
-# # Print the result
-# print(sentence)
+print('Hello it\'s a great')
